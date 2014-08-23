544ea236c828588124d8814141ded3c733d418d2
Danial Farid <dan@Danial.local>
2014-08-23 19:43:56 -0400

    editor

---
+++ src/main/java/com/df/javafiddle/CompilationErrorException.java
@@ -0,0 +1,18 @@
+package com.df.javafiddle;
+
+public class CompilationErrorException extends RuntimeException {
+	public String className;
+	public String errorJson;
+
+	public CompilationErrorException() {
+	}
+
+	public CompilationErrorException(String message) {
+		super(message);
+	}
+
+	public CompilationErrorException(String className, String errorJson) {
+		this.className = className;
+		this.errorJson = errorJson;
+	}
+}
